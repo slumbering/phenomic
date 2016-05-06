d1f01640fef664c3d4bfbf8c5e38dc170767e594
Khoa Nguyen <hi@khoanguyen.me>
2016-05-06 17:10:11 +0700

    Add a submit button

--- docs/web_modules/layouts/Search/index.js
+++ docs/web_modules/layouts/Search/index.js
@@ -1,17 +1,40 @@
 import React, { Component, PropTypes } from "react"
 // import styles from "./index.css"
+import { browserHistory } from "phenomic/lib/client"
+
+// Make some random string
+const makeId = () => {
+  let text = ""
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+  for (let i = 0; i < 5; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return text
+}
 
 export default class Homepage extends Component {
   static propTypes = {
     location: PropTypes.object.isRequired,
   };
 
+  handleButtonClick = () => {
+    browserHistory.push(`/search/?query=${ makeId() }`)
+  }
+
   render() {
     return (
-      <span>
-        { "Current query is" }
-        { this.props.location.search }
-      </span>
+      <div>
+        <span>
+          { "Current query is" }
+          { this.props.location.search }
+        </span>
+        <button
+          onClick={ this.handleButtonClick }
+        >
+          { "Submit me" }
+        </button>
+      </div>
     )
   }
 }
